51362471eba8f1405ee82ae5891685432b66dd11
ArchCC <acin@rambler.ru>
2012-04-20 22:15:31 +0400

    My first push

--- lib/ru/durdyev/wsadminextras/server/BaseWSAdminExtrasServer.py
+++ lib/ru/durdyev/wsadminextras/server/BaseWSAdminExtrasServer.py
@@ -12,7 +12,131 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import SocketServer
+import subprocess
+import logging
+import sys
+import collections
+import time
+import thread
 # base server
 
+from xml.dom.minidom import parse
+from ru.durdyev.wsadminextras.utils.XMLUtils import XMLUtils
+
 class BaseWSAdminExtrasServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
-    pass
+
+    #profile
+    _profile = None
+
+    #subprocess
+    p  = None
+
+    #List of commands
+    commandQueue = collections.deque()
+
+    def server_activate(self):
+        """Called by constructor to activate the server.
+
+        May be overridden.
+
+        """
+        self.server_activate()
+        self.startWsAdminProcess()
+
+    def finish_request(self, request, client_address):
+        """Finish one request by instantiating RequestHandlerClass."""
+        self.RequestHandlerClass(request, client_address, self)
+
+    def startWsAdminProcess(self):
+
+        # parsing xml config from profile
+        profile_config_file = open('../profiles/' + 'SimpleProfile' + '/config/' + 'config.xml')
+        xml_config = parse(profile_config_file)
+        parameters = {}
+
+        # was_home
+        hostElements = xml_config.getElementsByTagName('was_home')
+        parameters['was_home'] = hostElements[0].firstChild.nodeValue.strip()
+
+        # host address
+        hostElements = xml_config.getElementsByTagName('host')
+        parameters['host'] = hostElements[0].firstChild.nodeValue.strip()
+
+        # port number
+        portElements = xml_config.getElementsByTagName('port')
+        parameters['port'] =  portElements[0].firstChild.nodeValue.strip()
+
+        # username
+        userNameElements = xml_config.getElementsByTagName('username')
+        parameters['username'] = userNameElements[0].firstChild.nodeValue.strip()
+
+        # password element
+        passwordElements = xml_config.getElementsByTagName('password')
+        parameters['password'] = passwordElements[0].firstChild.nodeValue.strip()
+
+        # call deploy script to specific OS
+        if (sys.platform == 'win32'):
+            self.start_win(parameters)
+        else:
+            self.start_nix(parameters)
+
+        thread.start_new_thread(self.waitForCommand, ())
+
+        thread.start_new_thread(self.commandGenerator, ())
+
+    def waitForCommand(self):
+        while True:
+            out = self.p.stdout.readline()
+            print(out)
+            if(out[:9] == 'WASX7031I'):
+                print('end of loading')
+            if(out[:16] == 'wait for command' or out[:9] == 'WASX7031I'):
+                while True:
+                    if(len(self.commandQueue) > 0):
+                        command = self.commandQueue.pop()
+                        #p.stdin.write('execfile(\'C:\\work\\IDEFeatures\\Python\\WSAdminWrapper\\bin\\command.jy\')\n')
+                        self.p.stdin.write(command)
+                        self.p.stdin.flush()
+                        break
+                    else:
+                        print('wait 1 second')
+                        time.sleep(1)
+
+    def commandGenerator(self):
+        while True:
+            self.commandQueue.appendleft('execfile(\'c:\\work\\command.jy\')\n')
+            time.sleep(5)
+
+    # deploy files to windows
+    def start_win(self, parameters):
+        # trying to deploy all files
+        logging.info("Trying to deploy all files in %s temp directory", self.profile)
+
+        # getting parameters from profile configuration file.
+        try:
+            #TODO change host-port connection to profile
+            self.p = subprocess.Popen(parameters['was_home'] + "\\bin\\wsadmin.bat -host " + parameters['host'] + " -port " + parameters['port'] + " -user " + parameters['username'] + " -password " + parameters['password'] + " -lang jython ", shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE)
+
+        except IOError as e:
+            logging.info(e.strerror)
+            print("Cant't configuration file in profile %s " % self.profile)
+            #trying to deploy list of specific files
+
+    #deploy files to unix
+    def start_nix(self, parameters):
+        # trying to deploy all files
+        logging.info("Trying to deploy all files in %s temp directory", self.profile)
+
+        # getting parameters from profile configuration file.
+        try:
+            #TODO change host-port connection to profile
+            self.p = subprocess.Popen(parameters['was_home'] + "\\bin\\wsadmin.sh -host " + parameters['host'] + " -port " + parameters['port'] + " -user " + parameters['username'] + " -password " + parameters['password'] + " -lang jython ", shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE)
+
+        except IOError as e:
+            logging.info(e.strerror)
+            print("Cant't configuration file in profile %s " % self.profile)
+            #trying to deploy list of specific files
+
+    @property
+    def profile(self):
+        return self._profile
